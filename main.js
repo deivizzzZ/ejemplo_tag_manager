429d033dc982efb5a6564ab459fa27c233e9593f
David Alvarez <94860520+deivizzzZ@users.noreply.github.com>
2022-03-15 15:56:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let clicksCounter = 0;
+function increaseCounter() {
+    clicksCounter++;
+    document.getElementById("clicks-number").value = clicksCounter;
+}
+function sendClicks() {
+    // AQUÍ IRÁ LA LLAMADA A ANALYTICS
+    // gtag('event', 'clicks_sent', {
+    //     'clicks_number': document.getElementById("clicks-number").value
+    // });
+}
+
+function sendForm() {
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    // AQUÍ IRÁ LA LLAMADA A ANALYTICS
+    // gtag('event', 'form_sent', {'form_name': `${fname} ${lname}`});
+}
+
+document.querySelectorAll("a").forEach(link => {
+    link.addEventListener('click', () => {
+        // AQUÍ IRÁ LA LLAMADA A ANALYTICS
+        // gtag('event', 'link_clicked', {'link_visited': link.innerText});
+    });
+});

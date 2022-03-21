34afc7cbab98a664f4654fd59c3ddb4a82c4c7e1
David Alvarez <94860520+deivizzzZ@users.noreply.github.com>
2022-03-21 10:20:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function increaseCounter() {
 }
 function sendClicks() {
     dataLayer.push({
-        'event': 'clicks_sent',
+        // Estos datos se envían mediante
         'clicks_number': document.getElementById("clicks-number").value
     });
 }
@@ -20,18 +20,10 @@ function sendForm() {
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
     dataLayer.push({
-        // 'event': 'form_sent',
+        // Estos datos se envían mediante el activador 'envío de formulario' en GTM
+
         'form_name': `${fname} ${lname}`
     });
 }
 
 // Los datos de links se envían a través de variables predefinidas de GTM
-
-// document.querySelectorAll("a").forEach(link => {
-//     link.addEventListener('click', () => {
-//         dataLayer.push({
-//             'event': 'link_clicked',
-//             'link_visited': link.innerText
-//         });
-//     });
-// });
